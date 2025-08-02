7532c84f19e11d0d3a8e4e6f6652626ba652d4cc
Fabricio333 <53199599+Fabricio333@users.noreply.github.com>
2025-08-02 09:10:12 -0300

    Redirect Spanish previews to translated notes

--- app/page.tsx
+++ app/page.tsx
@@ -328,6 +328,8 @@ export default function HomePage() {
                 href={
                   post.type === "garden"
                     ? `/digital-garden/${post.id}`
+                    : locale === 'es'
+                    ? `/blog/${post.id}?lang=es`
                     : `/blog/${post.id}`
                 }
                 className="group block"
